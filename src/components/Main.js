3d16e109cbdedcadf07f5f19e56e89169c913030
chess2022 <lakeshore.hnh@gmail.com>
2022-05-25 18:43:40 -0700

    Sets up router in Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Index from "../pages/Index"
+import Show from "../pages/Show"
+
+export default function Main(props) {
+  return (
+  <main>
+    <Routes>
+      <Route exact path="/" element={<Index />} />
+      <Route path="/people/:id" element={<Show/> } />
+    </Routes>
+  </main>
+  )
+}
